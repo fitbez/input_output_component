915bebf3075667f599622ea0050b440ec22c6b35
fitsum belayneh <fitsu_alt@yahoo.com>
2019-07-03 16:57:29 -0400

    Userinput and output components are created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Userinput from "./Userinputs/Userinput";
+import UserOutput from "./UserOutputs/UserOutput";
 
-function App() {
-  return (
-    <div className="App">
-      <Userinput />
-    </div>
-  );
+class App extends Component {
+  state = {
+    username: "Fitsum Belayneh"
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      username: "Beza Akilu"
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Userinput username="Fitsum Belayneh" />
+        <UserOutput name="Belayneh" />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/UserOutputs/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserOutput = () => {
+  return (
+    <div>
+      <p>I am studying modern React.</p>
+      <p>React is an awesome UI library.</p>
+    </div>
+  );
+};
+
+export default UserOutput;
